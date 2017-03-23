fbd2c4fb27b6e26fee91ac3799dd0d0e74187739
Michael Brew <michael@onerent.co>
2017-03-22 19:45:15 -0700

    Add some jQuery to quickly throw info on screen

--- public/js/index.js
+++ public/js/index.js
@@ -19,18 +19,60 @@ function getWallPosts() {
   })
 }
 
-function loginClickHandler() {
-  login({scope: 'user_posts'})
-    .then(() => getWallPosts())
-    .then(posts => new Promise(resolve =>
-      $.ajax({
-        type: 'POST',
-        url: '/posts/analyze',
-        data: {posts: JSON.stringify(posts)},
-        dataType: 'json',
-        success: resolve
-      })
-    ))
-    .then(console.log)
-    .catch(console.err)
+function insertPostsTable(posts) {
+  const table = document.createElement('table')
+
+  const headerRow = document.createElement('th')
+
+  const messageTitleCell = document.createElement('td')
+    .appendChild(document.createTextNode('Post'))
+  const analysisTitleCell = document.createElement('td')
+    .appendChild(document.createTextNode('Sentiment Score'))
+
+  headerRow.appendChild(messageTitleCell)
+  headerRow.appendChild(analysisTitleCell)
+
+  table.appendChild(headerRow)
+
+  for (const post of posts) {
+    const row = document.createElement('tr')
+
+    let analysisScore = ':|'
+
+    if (post.sentiment.score < 0) {
+      analysisScore = ':('
+    } else if (post.sentiment.score > 0) {
+      analysisScore = ':)'
+    }
+
+    const messageNode = document.createTextNode(post.message)
+    const analysisNode = document.createTextNode(analysisScore)
+
+    const postCell = document.createElement('td').appendChild(messageNode)
+    const analysisCell = document.createElement('td').appendChild(analysisNode)
+
+    row.appendChild(postCell)
+    row.appendChild(analysisCell)
+
+    table.appendChild(row)
+  }
+
+  document.body.appendChild(table)
+}
+
+async function loginClickHandler() {
+  await login({scope: 'user_posts'})
+
+  const posts = await getWallPosts()
+  const analyzedPosts = await new Promise(resolve =>
+    $.ajax({
+      type: 'POST',
+      url: '/posts/analyze',
+      data: {posts: JSON.stringify(posts.filter(({message}) => message != null))},
+      dataType: 'json',
+      success: resolve
+    })
+  )
+
+  insertPostsTable(analyzedPosts)
 }
