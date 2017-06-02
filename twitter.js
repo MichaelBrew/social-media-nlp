c48cee7f2af3bc3b2aea69f265dcdd400ff6976c
Michael Brew <michael@onerent.co>
2017-06-01 22:24:16 -0700

    Add streaming API integration

--- twitter.js
+++ twitter.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config();
 
 const Twitter = require('twitter');
 const sentiment = require('sentiment');
@@ -7,29 +7,49 @@ const _ = require('lodash');
 const client = new Twitter({
   consumer_key: process.env.CONSUMER_KEY,
   consumer_secret: process.env.CONSUMER_SECRET,
-  bearer_token: process.env.BEARER_TOKEN
+  access_token_key: process.env.ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
-client
-  .get(`https://api.twitter.com/1.1/search/tweets.json?q=${process.env.KEYWORD}&src=typd`, {})
-  .then(res => {
-    const analyzedStatuses = res.statuses.map(({text}) => {
-      const {score} = sentiment(text)
-
-      let analysis = 'neutral'
-
-      if (score <= -5) {
-        analysis = 'very bad'
-      } else if (score < 0) {
-        analysis = 'bad'
-      } else if (score >= 5) {
-        analysis = 'very good'
-      } else if (score > 0) {
-        analysis = 'good'
+function startStreaming() {
+  client.stream('statuses/filter', {track: 'javascript'}, (stream) => {
+    stream.on('data', (event) => {
+      if (event) {
+        const {score} = sentiment(event.text);
+        console.log(`TEXT: ${event.text}; SCORE: ${score}`)
       }
+    });
 
-      return {text, sentiment: analysis}
-    })
-
-    console.log(analyzedStatuses)
+    stream.on('error', (error) => {
+      throw error;
+    });
   });
+}
+
+// TODO: Use application-only auth w/ bearer token
+function getKeywordTweets() {
+  client.get(`https://api.twitter.com/1.1/search/tweets.json?q=${process.env.KEYWORD}&src=typd`, {})
+    .then(res => {
+      const analyzedStatuses = res.statuses.map(({text}) => {
+        const {score} = sentiment(text)
+
+        let analysis = 'neutral'
+
+        if (score <= -5) {
+          analysis = 'very bad'
+        } else if (score < 0) {
+          analysis = 'bad'
+        } else if (score >= 5) {
+          analysis = 'very good'
+        } else if (score > 0) {
+          analysis = 'good'
+        }
+
+        return {text, sentiment: analysis}
+      })
+
+      console.log(analyzedStatuses)
+    });
+}
+
+startStreaming();
