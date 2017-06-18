6893c9cae0980c05377196634e96eb6023ad3b64
Michael Brew <michael@onerent.co>
2017-06-18 15:06:33 -0700

    Minor style cleanup

--- src/server.ts
+++ src/server.ts
@@ -1,56 +1,56 @@
-import app from './app';
 import * as http from 'http';
+import app from './app';
 
-const port = normalizePort(process.env.PORT || '3000')
-app.set('port', port)
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
 
 const server = http
   .createServer(app)
   .listen(port)
   .on('error', onError)
-  .on('listening', onListening)
+  .on('listening', onListening);
 
 function normalizePort(val) {
   const port = parseInt(val, 10)
 
   if (isNaN(port)) {
-    return val // named pipe
+    return val; // named pipe
   } else if (port >= 0) {
-    return port // port number
+    return port; // port number
   }
 
-  return false
+  return false;
 }
 
 function onError(error) {
   if (error.syscall !== 'listen') {
-    throw error
+    throw error;
   }
 
   const bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port
+    ? `Pipe ${port}`
+    : `Port ${port}`;
 
-  // handle specific listen errors with friendly messages
+  // Handle specific listen errors with friendly messages
   switch (error.code) {
     case 'EACCES':
-      console.error(bind + ' requires elevated privileges')
-      process.exit(1)
+      console.error(`${bind} requires elevated privileges`);
+      process.exit(1);
       break
     case 'EADDRINUSE':
-      console.error(bind + ' is already in use')
-      process.exit(1)
+      console.error(`${bind} is already in use`);
+      process.exit(1);
       break
     default:
-      throw error
+      throw error;
   }
 }
 
 function onListening() {
-  const addr = server.address()
+  const addr = server.address();
   const bind = typeof addr === 'string'
     ? `pipe ${addr}`
-    : `port ${addr.port}`
+    : `port ${addr.port}`;
 
-  console.log(`Listening on ${bind}`)
+  console.log(`Listening on ${bind}`);
 }
